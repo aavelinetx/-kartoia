d9418fa1c9ad387bd1f0e2a9ae0b5d3c015471a4
aavelinetx <93262290+aavelinetx@users.noreply.github.com>
2022-05-25 14:27:08 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+DFRobotMaqueenPlus.I2CInit()
+basic.forever(function () {
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1) {
+    	
+    }
+})
+basic.forever(function () {
+    let _3atala = 0
+    if (_3atala == 1) {
+        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
+        } else {
+            if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
+                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 140)
+                DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 30)
+            } else {
+                if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+                    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 140)
+                    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 30)
+                }
+            }
+        }
+    }
+})
